a4a8dc5f9b65185b50b0392098d18071d6d48fdf
Patrice Tisserand <p.tisserand@gmail.com>
2022-02-19 19:17:04 +0100

    contracts: added register and getAddress methods

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -4,7 +4,18 @@ pragma solidity ^0.8.10;
 import "hardhat/console.sol";
 
 contract Domains {
+    // To store data in blockchain
+    mapping (string => address) public domains;
     constructor() {
         console.log("This is Domains contract!");
+    }
+
+    function register(string calldata name) public {
+        domains[name] = msg.sender;
+        console.log("%s has registered domain: %s", msg.sender, name);
+    }
+
+    function getAddress(string calldata name) public view returns(address) {
+        return domains[name];
     }
 }
