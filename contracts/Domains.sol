c71f73e825cd38b5d69fce136f0f861ac5eb2c48
Patrice Tisserand <p.tisserand@gmail.com>
2022-02-19 22:30:20 +0100

    contracts: added setRecord & getRecord

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -5,17 +5,33 @@ import "hardhat/console.sol";
 
 contract Domains {
     // To store data in blockchain
-    mapping (string => address) public domains;
+    mapping(string => address) public domains;
+    mapping(string => string) public records;
+
     constructor() {
         console.log("This is Domains contract!");
     }
 
     function register(string calldata name) public {
+        require(domains[name] == address(0), "Domain is not free");
         domains[name] = msg.sender;
         console.log("%s has registered domain: %s", msg.sender, name);
     }
 
-    function getAddress(string calldata name) public view returns(address) {
+    function getAddress(string calldata name) public view returns (address) {
         return domains[name];
+    }
+
+    function setRecord(string calldata name, string calldata record) public {
+        require(domains[name] == msg.sender, "Only domain owner can set record");
+        records[name] = record;
+    }
+
+    function getRecord(string calldata name)
+        public
+        view
+        returns (string memory)
+    {
+        return records[name];
     }
 }
