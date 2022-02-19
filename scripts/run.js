45d10ba6819141f0411bef6fd171628f8f3b875b
Patrice Tisserand <p.tisserand@gmail.com>
2022-02-19 22:35:39 +0100

    scripts: added call to setRecord

--- scripts/run.js
+++ scripts/run.js
@@ -6,11 +6,15 @@ const main = async () => {
     console.log("Contract deployed to:", domainContract.address);
     console.log("Contract deployed by:", owner.address);
 
-    const txn = await domainContract.register("loki");
+    let txn = await domainContract.register("loki");
     await txn.wait();
 
     const domainOwner = await domainContract.getAddress("loki");
     console.log("Owner of domains: ", domainOwner);
+
+    // trying to set a record from another account
+    txn = await domainContract.connect(randomPerson).setRecord("loki", "It's mine now!");
+    await txn.wait();
 }
 
 const runMain = async () => {
