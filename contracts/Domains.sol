b0dcd6ffeb506e53d0851c7660bfc4e218385ef2
Patrice Tisserand <p.tisserand@gmail.com>
2022-02-20 17:19:05 +0100

    contracts: added price support when registering a domain

--- contracts/Domains.sol
+++ contracts/Domains.sol
@@ -1,19 +1,39 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
+import { StringUtils } from "./libraries/StringUtils.sol";
+
 import "hardhat/console.sol";
 
 contract Domains {
+    string public tld;
+
     // To store data in blockchain
     mapping(string => address) public domains;
     mapping(string => string) public records;
 
-    constructor() {
-        console.log("This is Domains contract!");
+    constructor(string memory _tld) payable {
+        tld = _tld;
+        console.log("Name service deployed: %s", tld);
     }
 
-    function register(string calldata name) public {
+    // to get price of domain according to name
+    function price(string calldata _name) public pure returns (uint256) {
+        uint256 len = StringUtils.strlen(_name);
+        require(len > 0);
+        if (len == 3) {
+            return 5 * 10**17; // 5 MATIC = 5 000 000 000 000 000 000 (18 decimals). We're going with 0.5 Matic cause the faucets don't give a lot
+        } else if (len == 4) {
+            return 3 * 10**17; // To charge smaller amounts, reduce the decimals. This is 0.3
+        } else {
+            return 1 * 10**17;
+        }
+    }
+
+    function register(string calldata name) public payable {
         require(domains[name] == address(0), "Domain is not free");
+        uint _price = price(name);
+        require(msg.value >= _price, "Not enough Matic pair");
         domains[name] = msg.sender;
         console.log("%s has registered domain: %s", msg.sender, name);
     }
@@ -23,7 +43,10 @@ contract Domains {
     }
 
     function setRecord(string calldata name, string calldata record) public {
-        require(domains[name] == msg.sender, "Only domain owner can set record");
+        require(
+            domains[name] == msg.sender,
+            "Only domain owner can set record"
+        );
         records[name] = record;
     }
 
